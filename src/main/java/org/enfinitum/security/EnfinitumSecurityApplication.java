5d4b6dc52e76bceeb2af1e092fc2b1718d5b0a21
Pushpendra Singh <pussingh5@publicisgroupe.net>
2019-06-11 15:50:30 +0530

    Updated versoin of spring-boot-starter-parent with latest

--- src/main/java/org/enfinitum/security/EnfinitumSecurityApplication.java
+++ src/main/java/org/enfinitum/security/EnfinitumSecurityApplication.java
@@ -4,7 +4,7 @@ import org.enfinitum.security.filter.GatewayFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
--- src/main/java/org/enfinitum/security/model/User.java
+++ src/main/java/org/enfinitum/security/model/User.java
@@ -15,7 +15,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Table(name = "user")
 public class User implements Serializable{
 
-
 	private static final long serialVersionUID = 1L;
 
 	@Id
